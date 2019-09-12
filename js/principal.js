3885aaccff412ab2bad41a80d18faf1de3f3e508
Thiago-Dev <thiagomiranda.tms@gmail.com>
2019-09-12 15:24:18 -0300

    add event preventDefault no clique do botão do form

--- js/principal.js
+++ js/principal.js
@@ -41,6 +41,7 @@ for(var i = 0; i < pacientes.length; i++) {
 
 var botaoAdicionar = document.querySelector('#adicionar-paciente');
 
-botaoAdicionar.addEventListener('click', function() {
-    console.log('teste de sistema');
+botaoAdicionar.addEventListener('click', function(event) {
+    event.preventDefault();
+    console.log('teste de sistem');
 });
