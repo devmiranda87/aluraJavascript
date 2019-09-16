56fc8bee69e412350319da00704644cad8d48ea5
Thiago-Dev <thiagomiranda.tms@gmail.com>
2019-09-16 15:24:28 -0300

    add obtemPacienteDoFormulario

--- js/form.js
+++ js/form.js
@@ -2,12 +2,12 @@ var botaoAdicionar = document.querySelector("#adicionar-paciente");
 botaoAdicionar.addEventListener("click", function(event) {
     event.preventDefault();
 
+    //BUSCANDO OBJETO DO FORMULARIO
     var form = document.querySelector("#form-adiciona");
 
-    var nome = form.nome.value;
-    var peso = form.peso.value;
-    var altura = form.altura.value;
-    var gordura = form.gordura.value;
+    //EXTRAINDO INFORMAÇÕES SOBRE O PACIENTE DO FORM
+    var paciente = obtemPacienteDoFormulario(form);
+
 
     var pacienteTr = document.createElement("tr");
 
@@ -17,18 +17,33 @@ botaoAdicionar.addEventListener("click", function(event) {
     var gorduraTd = document.createElement("td");
     var imcTd = document.createElement("td");
 
-    nomeTd.textContent = nome;
-    pesoTd.textContent = peso;
-    alturaTd.textContent = altura;
-    gorduraTd.textContent = gordura;
+    nomeTd.textContent = paciente.nome;
+    pesoTd.textContent = paciente.peso;
+    alturaTd.textContent = paciente.altura;
+    gorduraTd.textContent = paciente.gordura;
+    imcTd.textContent = calculaImc(paciente.peso, paciente.altura);
 
     pacienteTr.appendChild(nomeTd);
     pacienteTr.appendChild(pesoTd);
     pacienteTr.appendChild(alturaTd);
     pacienteTr.appendChild(gorduraTd);
+    pacienteTr.appendChild(imcTd);
 
     var tabela = document.querySelector("#tabela-pacientes");
 
     tabela.appendChild(pacienteTr);
 
 });
+
+function obtemPacienteDoFormulario(form)
+{
+    var paciente = {
+        nome: form.nome.value,
+        peso: form.peso.value,
+        altura: form.altura.value,
+        gordura: form.gordura.value,
+        imc: calculaImc(form.peso.value, form.altura.value)
+    }
+
+    return paciente;
+}
