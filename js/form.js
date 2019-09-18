f922ccc8b06a5297837b308bbe09c9280f2b227e
Thiago Miranda <thiagomiranda.tms@gmail.com>
2019-09-18 00:37:47 -0300

    patch na montaTr

--- js/form.js
+++ js/form.js
@@ -36,8 +36,8 @@ function montaTr(paciente)
     pacienteTr.appendChild(montaTd(paciente.nome, 'info-nome'));
     pacienteTr.appendChild(montaTd(paciente.peso, 'info-peso'));
     pacienteTr.appendChild(montaTd(paciente.altura, 'info-altura'));
-    pacienteTr.appendChild(montaTd(paciente.gordura, 'info-gordura'););
-    pacienteTr.appendChild(montaTd(paciente.imc, 'info-imc'););
+    pacienteTr.appendChild(montaTd(paciente.gordura, 'info-gordura'));
+    pacienteTr.appendChild(montaTd(paciente.imc, 'info-imc'));
 
     return pacienteTr;
 }
