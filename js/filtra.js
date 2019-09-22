7a8a5e3dd83580c2b3ca288daec810e6d4eb3364
Thiago Miranda <thiagomiranda.tms@gmail.com>
2019-09-22 12:19:35 -0300

    logica de filtro inicial

--- js/filtra.js
+++ js/filtra.js
@@ -1,11 +1,18 @@
 var campoFiltro = document.querySelector('#filtrar-tabela');
 
 campoFiltro.addEventListener('input', function() {
-    console.log(this.value);
+    var textoFiltro = this.value;
 
     var pacientes = document.querySelectorAll('.paciente');
     pacientes.forEach(function(paciente) {
+        var tdNome = paciente.querySelector('.info-nome');
+        var nome = tdNome.textContent;
 
+        if (nome != textoFiltro) {
+            paciente.classList.add('invisivel');
+            return;
+        }
+
+        paciente.classList.remove('invisivel');
     });
-
 });
