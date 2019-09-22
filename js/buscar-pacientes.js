5c33e83f3eb87d2d054a7d488030810415f0963e
Thiago Miranda <thiagomiranda.tms@gmail.com>
2019-09-22 18:18:38 -0300

    add requisicao ajax com tratativa de erro

--- js/buscar-pacientes.js
+++ js/buscar-pacientes.js
@@ -2,15 +2,23 @@ var botaoAdicionar = document.querySelector('#adicionar-pacientes');
 
 botaoAdicionar.addEventListener('click', function(){
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://api-pacientes.herokuapp.com/pacientes');
+    xhr.open('GET', 'https://api-pacientes.herokuapp.com/pacientesxx');
 
     xhr.addEventListener('load', function(){
+        var erroAjax = document.querySelector('#erro-ajax');
+
+        if(xhr.status != 200) {
+            erroAjax.classList.remove('invisivel');
+            return;
+        }
+
+        erroAjax.classList.add('invisivel');
         var resposta = xhr.responseText;
-        console.log(resposta);
-        console.log(typeof resposta);
         var pacientes = JSON.parse(resposta);
-        console.log(pacientes);
-        console.log(typeof pacientes);
+
+        pacientes.forEach(function(paciente) {
+            adicionarPacienteNaTabela(paciente);
+        });
     });
 
     xhr.send();
