b8eed01181323b19ae46306127ade17faa1c07ce
Thiago Miranda <thiagomiranda.tms@gmail.com>
2019-09-22 18:20:42 -0300

    patch link da api

--- js/buscar-pacientes.js
+++ js/buscar-pacientes.js
@@ -2,7 +2,7 @@ var botaoAdicionar = document.querySelector('#adicionar-pacientes');
 
 botaoAdicionar.addEventListener('click', function(){
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://api-pacientes.herokuapp.com/pacientesxx');
+    xhr.open('GET', 'https://api-pacientes.herokuapp.com/pacientes');
 
     xhr.addEventListener('load', function(){
         var erroAjax = document.querySelector('#erro-ajax');
